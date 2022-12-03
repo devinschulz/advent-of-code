31fa9c5362f2ac97da6571437b4a77e47464c39d
Devin Schulz <devin@devinschulz.com>
2022-12-03 12:29:05 -0500

    Day 3 2022

---
+++ 2022/3/index.ts
@@ -0,0 +1,46 @@
+import { assert, path } from "../deps.ts";
+
+const __dirname = path.dirname(path.fromFileUrl(import.meta.url));
+const text = await Deno.readTextFile(path.join(__dirname, "./input.txt"));
+
+const formatted = text.split("\n");
+
+const score = (acc: number, current: string | undefined) => {
+  if (!current) return acc;
+  return (
+    acc +
+    current.toLowerCase().charCodeAt(0) -
+    96 +
+    (/[A-Z]/.test(current) ? 26 : 0)
+  );
+};
+
+const find = (...arr: string[]) => {
+  return arr
+    .map((y) => y.split(""))
+    .reduce((a, b) => a.filter((c) => b.includes(c)))[0];
+};
+
+const part1 = formatted
+  .map((x) => {
+    const half = x.length / 2;
+    return find(x.slice(0, half), x.slice(half));
+  })
+  .reduce(score, 0);
+
+const SIZE = 3;
+
+const part2 = Array.from(
+  { length: Math.ceil(formatted.length / SIZE) },
+  (_v, i) => formatted.slice(i * SIZE, i * SIZE + SIZE),
+)
+  .map((x) => find(...x))
+  .reduce(score, 0);
+
+Deno.test("Part 1", () => {
+  assert.equal(part1, 8039);
+});
+
+Deno.test("Part 2", () => {
+  assert.equal(part2, 2510);
+});
